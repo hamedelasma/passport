24be96b21df0bbba05db35310a6a612d773fab6b
Hamed EL-Asma <hamedelasma@gmail.com>
2021-06-07 12:48:19 +0200

    configure author and book controllers

---
+++ app/Http/Controllers/api/AuthorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+
+    public function register(Request $request){
+
+    }
+    public function login(Request $request){
+
+    }
+    public function profile(){
+
+    }
+    public function logout(){
+
+    }
+}
---
+++ app/Http/Controllers/api/BookController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function create(Request $request){
+
+    }
+    public function index(){
+
+    }
+    public function show($id){
+
+    }
+    public function update(Request $request,$id){
+
+    }
+    public function delete($id){
+
+    }
+}
