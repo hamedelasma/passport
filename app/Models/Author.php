adad140edb0197fe5d5dc454f0b3030e156514c0
Hamed EL-Asma <hamedelasma@gmail.com>
2021-06-07 12:07:08 +0200

    configure author model and make it the defualt table for auth

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class Author extends Model
 {
-    use HasFactory;
+    use HasFactory ,HasApiTokens;
+    protected $hidden =[
+        'created_at','updated_at'
+    ];
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    protected $hidden =[
+        'created_at','updated_at'
+    ];
 }
