eb458c26d24f1e5efd4862b44910b0cc79061df7
Hamed EL-Asma <hamedelasma@gmail.com>
2021-06-07 15:12:17 +0200

    add create book function

--- app/Http/Controllers/api/BookController.php
+++ app/Http/Controllers/api/BookController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function create(Request $request){
+// validation
+        $request->validate([
+            "title" => "required",
+            "book_cost" => "required"
+        ]);
 
+        // create book data
+        $book = new Book();
+
+        $book->author_id = auth()->user()->id;
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->book_cost = $request->book_cost;
+
+        // save
+        $book->save();
+
+        // send response
+        return response()->json([
+            "status" => 1,
+            "message" => "Book created successfully"
+        ]);
     }
     public function index(){
 
